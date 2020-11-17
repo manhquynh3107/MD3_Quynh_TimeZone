3e0a21ae1ef9e30091832d802e31b68e0c65cbd1
manhquynh3107 <manhquynh3107@gmail.com>
2020-11-17 13:50:42 +0700

    Time Zone Laravel

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/{timezone?}', function ($timezone = null) {
+    if (!empty($timezone)) {
+        // Khởi tạo giá trị giờ theo múi giờ UTC
+        $time = new DateTime(date('Y-m-d H:i:s'), new DateTimeZone('UTC'));
+        // Thay đổi về múi giờ được chọn
+        $time->setTimezone(new DateTimeZone(str_replace('-', '/', $timezone)));
+        // Hiển thị giờ theo định dạng mong muốn
+        echo 'Time zone of your choice ' . $timezone . ' is currently: ' . $time->format('d-m-Y H:i:s');
+    }
+    return view('index');
+});
